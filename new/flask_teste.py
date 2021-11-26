96a0b5458f5ed14f161b227b4f29ad790b570df4
pji110-2021grupo5 <pji110.2021grupo5@gmail.com>
2021-11-26 13:02:09 -0300

    atulização p/ publicar no Heroku

--- new/flask_teste.py
+++ new/flask_teste.py
@@ -3,16 +3,21 @@ from flask import Flask
 from markupsafe import escape
 from flask import render_template
 from flask import request
-import os
+#import os
 #from sqlalchemy import SQLAlchemy
 import sqlite3
 from pathlib import Path
 
+from werkzeug.datastructures import T
+
 
 app = Flask(__name__)
 
-port = int(os.getenv('PORT'),'5000')
-app.run(host='0.0.0.0',port=port)
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+#port = int(os.getenv('PORT'),'5000')
+#app.run(host='0.0.0.0',port=port)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////univesp.db'
 #db = SQLAlchemy(app)
